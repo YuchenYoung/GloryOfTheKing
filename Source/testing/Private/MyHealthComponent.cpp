99116ae21dd6337ae0016518ca7a016e2b7c4302
YuchenYoung <1269689297@qq.com>
2019-06-14 00:08:43 +0800

    代码简单整理

--- Source/testing/Private/MyHealthComponent.cpp
+++ Source/testing/Private/MyHealthComponent.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyHealthComponent.h"
-#include"GameFramework/DamageType.h"
-#include"GameFramework/Controller.h"
-#include"TinyHero.h"
+#include "GameFramework/DamageType.h"
+#include "GameFramework/Controller.h"
+#include "TinyHero.h"
 
 // Sets default values for this component's properties
 UMyHealthComponent::UMyHealthComponent()
